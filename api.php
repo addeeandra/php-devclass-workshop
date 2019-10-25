9b76ebdc15c2a597708a80ed2ee43a8bf465110a
Aditya Chandra <inibukanadit@gmail.com>
2019-10-25 16:13:12 +0700

    add header json and login api

--- api.php
+++ api.php
@@ -2,6 +2,8 @@
 
 require_once 'connection.php';
 
+header('Content-Type: application/json');
+
 if (isset($_GET['action']) && $_GET['action'] == 'get_users') {
 
 	$query = $pdo->query('SELECT * FROM users');
@@ -12,4 +14,27 @@ if (isset($_GET['action']) && $_GET['action'] == 'get_users') {
 	}
 
 	echo json_encode($users);
+}
+
+if (isset($_GET['action']) && $_GET['action'] == 'get_token') {
+
+	if (!isset($_POST['username']) || !isset($_POST['password'])) {
+		echo json_encode([
+			'message' => 'Username dan password harus diisi'
+		]);
+		die();
+	}
+
+	$stmt = $pdo->prepare('SELECT * FROM users WHERE username = :username AND password = :password');
+	$stmt->bindParam(':username', $_POST['username']);
+	$stmt->bindParam(':password', $_POST['password']);
+	$stmt->execute();
+
+	while ($row = $stmt->fetch()) {
+		echo json_encode([
+			'api_key' => $row['api_key']
+		]);
+		die();
+	}
+
 }
