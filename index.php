7ca185cc9f2dd882823ff85d4b3afbaea7ed1f15
Aditya Chandra <inibukanadit@gmail.com>
2019-10-25 15:23:11 +0700

    add login page

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'connection.php';
-
-$result = $dbc->query('SELECT * FROM users');
-
-foreach ($result as $user) {
-	echo $user['username'] . '<br/>';
-}
---
+++ login.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Halaman Login</title>
+	</head>
+	<body>
+		<form method="POST" action="">
+			<input type="text" placeholder="Ketik username Anda" />
+			<input type="text" placeholder="Ketik kata sandi Anda" />
+			<button>Submit</button>
+		</form>
+	</body>
+</html>
